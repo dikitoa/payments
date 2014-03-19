8d507e33b270f2d7ded10c955ac2665ab0ead782
igarcc01 <igarcc01@estudiantes.unileon.es>
2014-03-19 21:43:10 +0100

    Añadidos métodos a la clase CreditCard y añadida la clase PurseCard

--- ULE-Bank/src/es/unileon/Payments/CreditCard.java
+++ ULE-Bank/src/es/unileon/Payments/CreditCard.java
@@ -2,8 +2,48 @@ package es.unileon.Payments;
 
 public class CreditCard extends Card {
 	private StrategyCommissionCredit commission;
+	private int limitDebt;
 	
 	public CreditCard() {
 		super(CardType.CREDIT);
+		this.limitDebt = 1000;
 	}
+	
+	public CreditCard(int payroll) {
+		super(CardType.CREDIT);
+		this.limitDebt = payroll*3;
+	}
+	
+	/**
+	 * Devuelve la comisión de la tarjeta de crédito
+	 * @return
+	 */
+	public StrategyCommissionCredit getCommission() {
+		return commission;
+	}
+
+	/**
+	 * Cambia la comisión de la tarjeta de crédito por la que se indica
+	 * @param commission
+	 */
+	public void setCommission(StrategyCommissionCredit commission) {
+		this.commission = commission;
+	}
+
+	/**
+	 * Devuelve el límite de deuda de la tarjeta
+	 * @return
+	 */
+	public int getLimitDebt() {
+		return limitDebt;
+	}
+
+	/**
+	 * Cambia el límite de deuda por el que recibe
+	 * @param limitDebt
+	 */
+	public void setLimitDebt(int limitDebt) {
+		this.limitDebt = limitDebt;
+	}
+	
 }
---
+++ ULE-Bank/src/es/unileon/Payments/PurseCard.java
@@ -0,0 +1,9 @@
+package es.unileon.Payments;
+
+public class PurseCard extends Card {
+
+	public PurseCard() {
+		super(CardType.PURSE);
+	}
+
+}
