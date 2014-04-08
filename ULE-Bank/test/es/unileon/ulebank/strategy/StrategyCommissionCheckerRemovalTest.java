1875f3139705cb8e552871b77b73dbbf4550314f
mdineg00 <mdineg00@estudiantes.unileon.es>
2014-04-08 15:20:00 +0200

    Cheker y comissionshop

---
+++ ULE-Bank/test/es/unileon/ulebank/strategy/StrategyCommissionCheckerRemovalTest.java
@@ -0,0 +1,31 @@
+package es.unileon.ulebank.strategy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StrategyCommissionCheckerRemovalTest {
+
+	StrategyCommissionRenovateRevolving comision = new StrategyCommissionRenovateRevolving();
+	int owner;
+	float interest, quantity;
+	
+	@Before
+	public void SetUp(){
+		
+	}
+	
+	@Test
+	public void testCalculateCommissionInt() {
+		
+		
+	}
+
+	
+	@Test
+	public void testCalculateCommissionFloatFloat() {
+		fail("Not yet implemented");
+	}
+
+}
