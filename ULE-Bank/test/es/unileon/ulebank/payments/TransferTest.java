1b01ccd67af3feb90a14bdeacc1c053598ae3233
Rober de Castro <rdecar00@estudiantes.unileon.es>
2014-04-28 19:17:06 +0200

    Corregidos fallos y completada clase transfer. Añadido código necesario en account.

--- ULE-Bank/test/es/unileon/ulebank/payments/TransferTest.java
+++ ULE-Bank/test/es/unileon/ulebank/payments/TransferTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import es.unileon.ulebank.exceptions.TransferException;
 import es.unileon.ulebank.payments.Transfer;
 
 /**
@@ -15,7 +16,7 @@ import es.unileon.ulebank.payments.Transfer;
 public class TransferTest {
 
 	//TODO Implementar cuando esté conectada con Account
-	/*public Account senderAccount;
+	public Account senderAccount;
 	public Account receiverAccount;
 	public float quantity;
 	Transfer transfer;
@@ -28,11 +29,11 @@ public class TransferTest {
 	}
 	
 	@Test
-	public void test() {
+	public void test() throws TransferException {
 		float beforeMoney = this.receiverAccount.getBalance();
 		this.transfer = new Transfer(this.senderAccount, this.receiverAccount, this.quantity);
 		float afterMoney = this.receiverAccount.getBalance();
 		assertEquals(beforeMoney - afterMoney, this.quantity, 0.001);
-	}*/
+	}
 
 }
