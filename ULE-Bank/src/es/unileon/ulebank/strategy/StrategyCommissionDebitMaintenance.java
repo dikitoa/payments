fe96ff538ed48a9d9e5f4088a8584f0f82dd5b31
Rober de Castro <rdecar00@estudiantes.unileon.es>
2014-05-01 23:49:55 +0200

    Corregidas comisiones, añadido archivos txt con las comisiones, edades e intereses por defecto de las tarjetas.

--- ULE-Bank/src/es/unileon/ulebank/strategy/StrategyCommissionDebitMaintenance.java
+++ ULE-Bank/src/es/unileon/ulebank/strategy/StrategyCommissionDebitMaintenance.java
@@ -1,5 +1,11 @@
 package es.unileon.ulebank.strategy;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import es.unileon.ulebank.exceptions.CommissionException;
 import es.unileon.ulebank.payments.Client;
 
@@ -13,17 +19,24 @@ public class StrategyCommissionDebitMaintenance implements StrategyCommissionDeb
 
 	private Client owner;
 	private float quantity;
-	private final int MAXIMUM_AGE = 26;
-	private final float DEFAULT_COMMISSION = 15;
+	private int maximum_age;
+	private float default_commission;
+	private String AGE = "debit_age";
+	private String COMMISSION = "debit_maintenance";
 	
 	/**
 	 * Class constructor
 	 * @param owner
 	 * @param quantity
 	 * @throws CommissionException 
+	 * @throws IOException 
+	 * @throws NumberFormatException 
 	 */
-	public StrategyCommissionDebitMaintenance(Client owner, float quantity) throws CommissionException{
+	public StrategyCommissionDebitMaintenance(Client owner, float quantity) throws CommissionException, NumberFormatException, IOException{
 		this.owner = owner;
+		this.setDefaultCommission();
+		this.setMaximumAge();
+		
 		if (quantity >= 0)
 			this.quantity = quantity;
 		else
@@ -32,10 +45,108 @@ public class StrategyCommissionDebitMaintenance implements StrategyCommissionDeb
 
 	@Override
 	public float calculateCommission() {
-		if (this.owner.getAge() >= this.MAXIMUM_AGE)
-			return this.DEFAULT_COMMISSION;
+		if (this.owner.getAge() > this.maximum_age)
+			return this.default_commission;
 		else
 			return this.quantity;
 	}
 
+	/**
+	 * Setter of maximum_age
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	private void setMaximumAge() throws NumberFormatException, IOException{
+		FileReader fileReader = null;
+		BufferedReader bufferReader = null;
+
+		try {
+			// Open file
+			fileReader = new FileReader ("etc/maximum_ages.txt");
+			bufferReader = new BufferedReader(fileReader);
+
+			// Read file
+			String line;
+			while((line = bufferReader.readLine())!=null){
+				StringTokenizer stringTokenizer = new StringTokenizer(line);
+				String[] aux = new String[2];
+				int i = 0;
+				
+				//Obtains the line's elements separately in the aux array 
+				while (stringTokenizer.hasMoreElements()) {
+					aux[i] = stringTokenizer.nextToken();
+					i++;
+				}
+				
+				//Check if the line is the correct one
+				if (aux[0].compareTo(this.AGE) == 0){
+					this.maximum_age = Integer.parseInt(aux[1]);
+					break;
+				}
+			}
+			
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}finally{
+			//Close file
+			try{                    
+				if( fileReader != null ){   
+					fileReader.close();     
+				}                  
+			}catch (IOException e2){ 
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Setter of default_commission
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	private void setDefaultCommission() throws NumberFormatException, IOException{
+		FileReader fileReader = null;
+		BufferedReader bufferReader = null;
+
+		try {
+			// Open file
+			fileReader = new FileReader ("etc/card_commissions.txt");
+			bufferReader = new BufferedReader(fileReader);
+
+			// Read file
+			String line;
+			while((line = bufferReader.readLine())!=null){
+				StringTokenizer stringTokenizer = new StringTokenizer(line);
+				String[] aux = new String[2];
+				int i = 0;
+				
+				//Obtains the line's elements separately in the aux array 
+				while (stringTokenizer.hasMoreElements()) {
+					aux[i] = stringTokenizer.nextToken();
+					i++;
+				}
+				
+				//Check if the line is the correct one
+				if (aux[0].compareTo(this.COMMISSION) == 0){
+					this.default_commission = Float.parseFloat(aux[1]);
+					break;
+				}
+			}
+			
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}finally{
+			//Close file
+			try{                    
+				if( fileReader != null ){   
+					fileReader.close();     
+				}                  
+			}catch (IOException e2){ 
+				e2.printStackTrace();
+			}
+		}
+	}
+	
 }
