deae5a8d50c71a20c4d3be9d23ae8a96b33f5b97
mdineg00 <mdineg00@estudiantes.unileon.es>
2014-05-03 15:48:35 +0200

    Correción y actualización de test

--- ULE-Bank/test/es/unileon/ulebank/strategy/StrategyCommissionDebitMaintenanceTest.java
+++ ULE-Bank/test/es/unileon/ulebank/strategy/StrategyCommissionDebitMaintenanceTest.java
@@ -1,11 +1,13 @@
 package es.unileon.ulebank.strategy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; 
+
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import es.unileon.ulebank.payments.Card;
+import es.unileon.ulebank.exceptions.CommissionException;
 import es.unileon.ulebank.payments.Client;
 
 
@@ -17,66 +19,94 @@ import es.unileon.ulebank.payments.Client;
 public class StrategyCommissionDebitMaintenanceTest {
 
 	private Client owner;
-	private Card card;
 	private float quantity;
-	private final float DEFAULT_COMMISSION = 15;
-	StrategyCommissionDebitMaintenance emision;
+	private float default_commission = 15;
+	StrategyCommissionDebitMaintenance commission;
 	
 	/**
-	 * Inicialiamos las variables
+	 * Inicializamos las variables
+	 * @throws IOException 
+	 * @throws CommissionException 
+	 * @throws NumberFormatException 
 	 */
 	
 	@Before
-	public void SetUp(){
+	public void SetUp() throws NumberFormatException, CommissionException, IOException{
 	
-		owner = new Client();
-		quantity = 1500;
-		emision = new StrategyCommissionDebitMaintenance(owner, card, quantity);
+		quantity = 15;
+		owner.setAge(20);
+		commission = new StrategyCommissionDebitMaintenance(owner, quantity);
 	}
 	
-	/**
-	 * Comprobamos que si el cliente tiene menos de 26 anos 
-	 * su comision no consta.
-	 */
-	
-	@Test
-	public void testCalculateCommission1() {
-		
-		owner.setAge(18);
-		emision = new StrategyCommissionDebitMaintenance(owner, card, quantity);
-		assertTrue(emision.calculateCommission()!=0);
-	}
 
-	
+	/**
+	 * Comprobamos que si tiene menos de 25 anos, la comision es quantity
+	 */
 	@Test
-	public void testCalculateCommission2() {
+	public void testCalculateCommissionOwnerLessThanMaximunAge() {
 		
-		owner.setAge(18);
-		emision = new StrategyCommissionDebitMaintenance(owner, card, quantity);
-		assertTrue(emision.calculateCommission()==quantity);
+		assertTrue(commission.calculateCommission()==quantity);
+	}
+	
+
+	/**
+	 * Comprobamos que si tiene menos de 25 anos, la comision es quantity
+	 */
+	@Test
+	public void testCalculateCommissionOwnerLessThanMaximunAgeFalse() {
+		
+		assertFalse(commission.calculateCommission()!=quantity);
 	}
 	
 	
 	/**
-	 * Comprobamos que para un cliente mayor de 26 anos 
-	 * su emision de comision es la cuantia mas la comision.
+	 * Comprobamos que si tiene más de 25 anos, la comision es la estipulada por defecto
+	 * @throws IOException 
+	 * @throws CommissionException 
+	 * @throws NumberFormatException 
 	 */
 	@Test
-	public void testCalculateCommission3() {
-		
+	public void testCalculateCommissionOwnerMoreThanMaximunAge() throws NumberFormatException, CommissionException, IOException {
 		owner.setAge(30);
-		emision = new StrategyCommissionDebitMaintenance(owner, card, quantity);
-		assertFalse(emision.calculateCommission()==0);
+		commission = new StrategyCommissionDebitMaintenance(owner, quantity);
+		assertTrue(commission.calculateCommission()==default_commission);
 	}
-
+		
 	
+	/**
+	 * Comprobamos que si tiene más de 25 anos, la comision es la estipulada por defecto
+	 * @throws IOException 
+	 * @throws CommissionException 
+	 * @throws NumberFormatException 
+	 */
 	@Test
-	public void testCalculateCommission4() {
-		
+	public void testCalculateCommissionOwnerMoreThanMaximunAgeFalse() throws NumberFormatException, CommissionException, IOException {
 		owner.setAge(30);
-		emision = new StrategyCommissionDebitMaintenance(owner, card, quantity);
-		assertTrue(emision.calculateCommission()==(DEFAULT_COMMISSION+quantity));
+		commission = new StrategyCommissionDebitMaintenance(owner, quantity);
+		assertFalse(commission.calculateCommission()!=default_commission);
 	}
 	
-
+	
+	/**
+	 * Comprueba que se lanza la excepcion de la comision correctamente por el metodo try/catch
+	 */
+	@Test
+	public void testCalculateNegativeCommission() {
+		
+		quantity = -10;
+		try {
+			commission = new StrategyCommissionDebitMaintenance(owner, quantity);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CommissionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
