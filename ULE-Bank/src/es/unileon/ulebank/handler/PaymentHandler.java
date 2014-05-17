b9e571e56f3f2f6583db19f5ef3ddbbd0df2dcdc
Rober de Castro <rdecar00@estudiantes.unileon.es>
2014-05-17 16:02:08 +0200

    Test de PaymentHandler añadidos

--- ULE-Bank/src/es/unileon/ulebank/handler/PaymentHandler.java
+++ ULE-Bank/src/es/unileon/ulebank/handler/PaymentHandler.java
@@ -1,34 +1,33 @@
 package es.unileon.ulebank.handler;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import es.unileon.ulebank.exceptions.PaymentHandlerException;
 
 /**
  * Class of Payment Handler
  * @author Rober dCR
  * @date 9/04/2014
- * @brief Identifier of Payment formed by the 5 last numbers of Card Handler,
- * 3 first chars from concept (if concept is empty it will be BBB) and 4 numbers of the month and year
- * of the payment (example 30th April of 2014 -> 0414).
+ * @brief Identifier of Payment formed by the 4 last numbers of Card Handler,
+ * and 11 numbers of the month, year and complete hour.
  */
 public class PaymentHandler implements Handler {
 
-	private final int LENGTH = 12;
-	private final int POSITION_CARD = 10;
-	private final int NUMBER_INITIALS = 5;
-	private final int NUMBER_MEDIUM = 3;
+	private final int LENGTH = 15;
+	private final int POSITION_CARD = 15;
+	private final int NUMBER_INITIALS = 4;
 	private String id;
 	
 	/**
 	 * Class constructor
 	 * @param cardNumber
-	 * @param concept
-	 * @param date
 	 * @throws PaymentHandlerException
 	 */
-	public PaymentHandler(String cardNumber, String concept, String date) throws PaymentHandlerException{
-		this.id = this.obtainInitials(cardNumber) + this.obtainChars(concept) + this.obtainFinals(date);
+	public PaymentHandler(Handler cardNumber, Date date) throws PaymentHandlerException{
+		this.id = this.obtainInitials(cardNumber) + this.obtainFinals(date);
 		if (this.id.length() != this.LENGTH)
-			throw new PaymentHandlerException("Longitud de Payment Handler err���nea");
+			throw new PaymentHandlerException("Length of payment handler incorrect.");
 	}
 
 	@Override
@@ -37,39 +36,26 @@ public class PaymentHandler implements Handler {
 	}
 
 	/**
-	 * @brief Method that obtains the first 5 numbers of the handler
+	 * @brief Method that obtains the first 4 numbers of the handler
 	 * @param cardNumber
-	 * @return 5 initials
+	 * @return 4 initials
 	 * @throws PaymentHandlerException
 	 */
-	private String obtainInitials(String cardNumber) throws PaymentHandlerException{
-		if (cardNumber.substring(this.POSITION_CARD).length() == this.NUMBER_INITIALS)
-			return cardNumber.substring(this.POSITION_CARD);
+	private String obtainInitials(Handler cardNumber) throws PaymentHandlerException{
+		if (cardNumber.toString().substring(this.POSITION_CARD).length() == this.NUMBER_INITIALS)
+			return cardNumber.toString().substring(this.POSITION_CARD);
 		else
 			throw new PaymentHandlerException("Longitud de los numeros iniciales incorrecta");
 	}
 	
 	/**
-	 * Method that obtains the 4 final numbers
+	 * Method that obtains the 11 final numbers
 	 * @param date
 	 * @return
 	 */
-	private String obtainFinals(String date){
-		//Format date dd/MM/yyyy HH:mm:ss
-		return date.substring(4, 5) + date.substring(9, 10);
-	}
-	
-	/**
-	 * Method that obtains the 3 characters of the medium
-	 * @param concept
-	 * @return 3 characters for the medium of the id
-	 */
-	private String obtainChars(String concept){
-		if (concept.length() < this.NUMBER_MEDIUM)
-			for (int i = concept.length(); i < this.NUMBER_MEDIUM; i++)
-				concept.concat("B");
-		
-		return concept.substring(0, 2);
+	private String obtainFinals(Date date){
+		SimpleDateFormat format = new SimpleDateFormat("MMyyHHmmSS");
+		return format.format(date);
 	}
 	
 	/**
