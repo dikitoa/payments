786e212eac3a0656e315795810a1ec891d5ecef7
Alvaro <Alvaro@192.168.1.109>
2014-06-14 17:07:58 +0200

    Delete duplicate class exception and reorganized imports

--- src/main/java/es/unileon/ulebank/payments/SecurityCardException.java
+++ src/main/java/es/unileon/ulebank/payments/SecurityCardException.java
@@ -1,26 +1,26 @@
-package es.unileon.ulebank.payments;
-
-/**
- * SecurityCardException Class
- * 
- * @author Rober dCR
- * @date 26/03/2014
- * @brief Exception that is thrown if the security conditions are not accepted
- */
-public class SecurityCardException extends Exception {
-
-    /**
-     * Version
-     */
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * Class constructor
-     * 
-     * @param message
-     */
-    public SecurityCardException(String message) {
-        super(message);
-    }
-
-}
+//package es.unileon.ulebank.payments;
+//
+///**
+// * SecurityCardException Class
+// * 
+// * @author Rober dCR
+// * @date 26/03/2014
+// * @brief Exception that is thrown if the security conditions are not accepted
+// */
+//public class SecurityCardException extends Exception {
+//
+//    /**
+//     * Version
+//     */
+//    private static final long serialVersionUID = 1L;
+//
+//    /**
+//     * Class constructor
+//     * 
+//     * @param message
+//     */
+//    public SecurityCardException(String message) {
+//        super(message);
+//    }
+//
+//}
