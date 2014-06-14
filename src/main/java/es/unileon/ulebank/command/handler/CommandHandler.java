63499fb60443c60bf2c5d4feff3037cb246750af
Alvaro <Alvaro@192.168.1.109>
2014-06-14 13:19:28 +0200

    conflicts are resolved

---
+++ src/main/java/es/unileon/ulebank/command/handler/CommandHandler.java
@@ -0,0 +1,105 @@
+package es.unileon.ulebank.command.handler;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import es.unileon.ulebank.handler.Handler;
+
+/**
+ * CommandHandler class
+ * 
+ * @author Rober dCR
+ * @date 9/04/2014
+ * @brief Class of the identifier for Commands
+ */
+public class CommandHandler implements Handler {
+
+    /**
+     * Identifier of the object which makes the command
+     */
+    private final Handler id;
+    /**
+     * Date when the commandHandler is created
+     */
+    private final String date;
+
+    /**
+     * Class constructor
+     * 
+     * @param handler
+     */
+    public CommandHandler(Handler handler) {
+        this.id = handler;
+        final DateFormat dateFormat = new SimpleDateFormat("ddMMyyyyHHmmssss");
+        this.date = dateFormat.format(new Date());
+    }
+
+    /**
+     * Compara el identificador actual con el que se indica
+     * 
+     * @param another
+     * @return devuelve un 0 si son iguales
+     * @return devuelve otro numero si son distintos
+     */
+    @Override
+    public int compareTo(Handler another) {
+        return this.toString().compareTo(another.toString());
+    }
+    
+	/**
+	 * Compare two identifiers and determine if are equals or not
+	 * 
+	 * @param another
+	 * @return true if are equals
+	 * @return false if aren't equals
+	 */
+	@Override
+	public boolean equals(Object another) {
+		if (another == null) {
+			return false;
+		}
+		
+		if (another.getClass() != getClass()) {
+			return false;
+		}
+		
+		Handler other = (Handler) another;
+		
+		if (this.toString().equals(other.toString())) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.hashCode();
+	}
+
+    /**
+     * Getter id
+     * 
+     * @return id
+     */
+    public Handler getId() {
+        return this.id;
+    }
+
+    /**
+     * Getter date
+     * 
+     * @return String
+     */
+    public String getDate() {
+        return this.date.toString();
+    }
+
+    /**
+     * Devuelve en una cadena de strings el id y la fecha
+     */
+    @Override
+    public String toString() {
+        return this.id.toString() + " " + this.date.toString();
+    }
+}
---
+++ src/main/java/es/unileon/ulebank/payments/handler/PaymentHandler.java
@@ -0,0 +1,126 @@
+package es.unileon.ulebank.payments.handler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import es.unileon.ulebank.handler.Handler;
+import es.unileon.ulebank.handler.MalformedHandlerException;
+
+/**
+ * Class of Payment Handler
+ * 
+ * @author Rober dCR
+ * @date 9/04/2014
+ * @brief Identifier of Payment formed by the 4 last numbers of Card Handler,
+ *        and 11 numbers of the month, year and complete hour.
+ */
+public class PaymentHandler implements Handler {
+
+	/**
+	 * Posicion del numero de tarjeta a partir de la cual obtenemos la subcadena
+	 */
+	private static final int POSITION_CARD = 15;
+
+	/**
+	 * Command Identifier
+	 */
+	private final String id;
+
+	/**
+	 * Class constructor
+	 * @param handler
+	 * @param date
+	 * @throws MalformedHandlerException
+	 */
+	public PaymentHandler(Handler handler, Date date) {
+		this.id = this.obtainInitials(handler) + this.obtainFinals(date);
+	}
+
+	/**
+	 * Compara el identificador actual con el que se indica
+	 * @param another
+	 * @return devuelve un 0 si son iguales
+	 * @return devuelve otro numero si son distintos
+	 */
+	@Override
+	public int compareTo(Handler another) {
+		return this.toString().compareTo(another.toString());
+	}
+	
+	/**
+<<<<<<< HEAD
+	 * Compare two identifiers and determine if are equals or not
+	 * 
+	 * @param another
+	 * @return true if are equals
+	 * @return false if aren't equals
+=======
+	 * Comprueba que dos handler son iguales
+	 * @param another
+	 * @return true si son iguales, false en cualquier otro caso
+>>>>>>> branch 'master' of git@github.com:ULE-Bank/payments.git
+	 */
+	@Override
+	public boolean equals(Object another) {
+		if (another == null) {
+			return false;
+		}
+		
+		if (another.getClass() != getClass()) {
+			return false;
+		}
+		
+		Handler other = (Handler) another;
+		
+		if (this.toString().equals(other.toString())) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.hashCode();
+	}
+
+	/**
+	 * @brief Method that obtains the first 4 numbers of the handler
+	 * @param cardNumber
+	 * @return 4 initials
+	 */
+	private String obtainInitials(Handler cardNumber) {
+		return cardNumber.toString().substring(PaymentHandler.POSITION_CARD);
+	}
+
+	/**
+	 * Method that obtains the 11 final numbers
+	 * @param date
+	 * @return final id of the handler
+	 */
+	private String obtainFinals(Date date) {
+		final SimpleDateFormat format = new SimpleDateFormat("MMyyHHmmSS");
+		if (format.format(date).length() < 11) {
+			return "0" + format.format(date);
+		} else {
+			return format.format(date);
+		}
+	}
+
+	/**
+	 * To String class method
+	 * @return this handler in string
+	 */
+	@Override
+	public String toString() {
+		return this.id;
+	}
+
+	/**
+	 * Getter of id
+	 * @return id
+	 */
+	public String getId() {
+		return this.id;
+	}
+
+}
