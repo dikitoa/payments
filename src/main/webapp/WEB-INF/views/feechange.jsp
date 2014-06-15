4dc290468802ee9d2efc42d5b0993b764c83b298
David Gómez Riesgo <David Gmez Riesgo@TOSHIBA>
2014-06-15 20:31:22 +0200

    Test ChangeFeesFormController and view feechange

--- src/main/webapp/WEB-INF/views/feechange.jsp
+++ src/main/webapp/WEB-INF/views/feechange.jsp
@@ -1,34 +1,63 @@
-<%@ include file="/WEB-INF/views/include.jsp"%>
+<%@ include file="/WEB-INF/views/include.jsp" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <html>
-<head>
-<title><fmt:message key="title"></fmt:message></title>
-</head>
-<body>
-	<h1>
-		<fmt:message key="comissions"></fmt:message>
-	</h1>
-	<h2></h2>
-
-	<br>
-	<table>
-			<tr>
-				<td>Comission Emission: </td>
-				<td><c:out value="${card.commissionEmission}"></c:out></td>
+	<head>
+		<style type="text/css">
+			<%@ include file ="/resources/css/style.css" %> 
+			.error { color: red; }
+		</style>
+		<title><fmt:message key="title" /></title>
+	</head>
+	<body>
+		<header>
+			<a class="backHome" href="<c:url value="startpage.htm"/>" title="Home"></a>
+			<a class="optionsHeader assets" href="<c:url value="assets.htm"/>" title="Assets"></a>
+			<a class="optionsHeader brokerage" href="<c:url value="brokerage.htm"/>" title="brokerage"></a>
+			<a class="optionsHeader liabilities" href="<c:url value="liabilities.htm"/>" title="Liabilities"></a>
+			<a class="optionsHeader optionsHeaderSelected payments" href="<c:url value="payments.htm"/>" title="Payments"></a>
+		</header>
+	
+		<nav class="menu">
+			<ul>
+				<li>
+					<a href="<c:url value=""/>">Opcion</a>
+				</li>
+				<li>
+					<a href="<c:url value=""/> ">Opcion</a>
+				</li>
+				<li>
+					<a href="<c:url value=""/>">Opcion</a>
+				</li>
+				<li>
+					<a href="<c:url value=""/>">Opcion</a>
+				</li>
+				<li>
+					<a href="<c:url value=""/>">Opcion</a>
+				</li>
+				<li>
+					<a href="<c:url value=""/>">Opcion</a>
+				</li>
+			</ul>
+		</nav>
 			
-			</tr>
-			<tr>
-				<td>Comission Maintenance: </td>
-				<td><c:out value="${card.commissionMaintenance}"></c:out></td> 
-			</tr>
-			<tr>
-				<td>Comission Renovate: </td>
-				<td><c:out value="${card.commissionRenovate}"></c:out></td> 
-			</tr>
-	</table>
-
-	<br>
-	<br>
-
-</body>
+		<h1><fmt:message key="comissions"/></h1>
+		
+		<form:form method="post" commandName="feeChange">
+  			<table width="95%" bgcolor="f8f8ff" border="0" cellspacing="0" cellpadding="5">
+	    		<tr>
+      				<td align="right" width="20%">Change Fee:</td>
+        			<td width="20%">
+	          			<form:input path="feeChange"/>
+        			</td>
+        			<td width="60%">
+	          			<form:errors path="feeChange" cssClass="error"/>
+        			</td>
+    			</tr>
+  			</table>
+  			<br>
+  			<input type="submit" value="Save Fee Change">
+		</form:form>
+		<a href="<c:url value="cards.htm"/>">Home</a>
+	</body>
 </html>
---
+++ src/test/java/es/unileon/ulebank/web/ChangeFeesFormControllerTest.java
@@ -0,0 +1,86 @@
+package es.unileon.ulebank.web;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.unileon.ulebank.account.Account;
+import es.unileon.ulebank.bank.Bank;
+import es.unileon.ulebank.bank.BankHandler;
+import es.unileon.ulebank.client.Client;
+import es.unileon.ulebank.client.Person;
+import es.unileon.ulebank.fees.FeeStrategy;
+import es.unileon.ulebank.fees.LinearFee;
+import es.unileon.ulebank.handler.GenericHandler;
+import es.unileon.ulebank.office.Office;
+import es.unileon.ulebank.payments.Card;
+import es.unileon.ulebank.payments.CreditCard;
+import es.unileon.ulebank.payments.handler.CardHandler;
+import es.unileon.ulebank.service.ChangeLimit;
+import es.unileon.ulebank.service.FeeChange;
+import es.unileon.ulebank.service.SimpleCardManager;
+import es.unileon.ulebank.utils.CardProperties;
+
+public class ChangeFeesFormControllerTest {
+    
+	private SimpleCardManager cardManager;
+    
+    Card testCard;
+	CardHandler handler;
+	private Office office;
+	private Bank bank;
+    private String accountNumber = "0000000000";
+    
+    @Before
+    public void setUp() throws Exception {
+    	this.cardManager = new SimpleCardManager();
+		CardProperties properties = new CardProperties();
+		properties.setCvvSize(3);
+		properties.setPinSize(4);
+		properties.setMinimumLimit(300.0);
+		properties.setExpirationYear(3);
+		this.bank = new Bank(new BankHandler("9876"));
+		this.office = new Office(new GenericHandler("9876"), this.bank);
+		handler = new CardHandler(new BankHandler("9876"), "06", "987654321");
+		Client client = new Person(71449133, 'R');
+		Account account = new Account(office, bank, accountNumber, client);
+		FeeStrategy commissionEmission = new LinearFee(0, 26);
+		FeeStrategy commissionMaintenance = new LinearFee(0, 0);
+		FeeStrategy commissionRenovate = new LinearFee(0, 0);
+		testCard = new CreditCard(handler, client, account);
+		testCard.setBuyLimitMonthly(1500.0);
+		testCard.setBuyLimitDiary(500.0);
+		testCard.setCashLimitMonthly(1500.0);
+		testCard.setCashLimitDiary(600.0);
+		testCard.setCommissionEmission(commissionEmission);
+		testCard.setCommissionMaintenance(commissionMaintenance);
+		testCard.setCommissionRenovate(commissionRenovate);
+		this.cardManager.setCard(this.testCard);
+	}
+    
+	@Test
+	public void testFormBackingObject() throws Exception{		
+	/**	ChangeFeesFormController controllerFee = new ChangeFeesFormController();		
+		controllerFee.setCardManager(this.cardManager);	
+		assertEquals(controllerFee.formBackingObject(null).getFeeChange(),this.cardManager.getCard().getFee(),0.01);
+		**/
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOnSubmit() throws Exception{		
+		ChangeFeesFormController controllerFee = new ChangeFeesFormController();		
+		controllerFee.setCardManager(this.cardManager);	
+		FeeChange fee= new FeeChange();
+		fee.setFeeChange(40);
+	}
+	
+	@Test
+	public void testGetCardManager() {
+		ChangeFeesFormController controllerFee = new ChangeFeesFormController();		
+		controllerFee.setCardManager(this.cardManager);	
+		assertEquals(this.cardManager, controllerFee.getCardManager());
+	}
+}
