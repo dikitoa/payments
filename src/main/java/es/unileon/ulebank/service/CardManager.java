4b2973c0e6e0d6a21fc0571dba9e0857d09abb75
Rober de Castro <rdecar00@estudiantes.unileon.es>
2014-06-15 13:50:02 +0200

    Añadidos los controladores para modificar los limites de la tarjeta.

---
+++ src/main/java/es/unileon/ulebank/service/CardManager.java
@@ -0,0 +1,37 @@
+package es.unileon.ulebank.service;
+
+import java.io.Serializable;
+
+import es.unileon.ulebank.payments.Card;
+
+/**
+ * Card Manager Interface
+ * @author Rober dCR
+ * @date 10/05/2014
+ * @brief Interface for the managers used to modify card attributes
+ */
+public interface CardManager extends Serializable {
+
+	/**
+	 * Method that changes the buy limits using the command with the limits from buyLimits.jsp
+	 * @param diary amount limit
+	 * @param monthly amount limit
+	 * @throws Exception 
+	 */
+    public void changeBuyLimits(double diary, double monthly) throws Exception;
+    
+    /**
+     * Method that changes the cash limits using the command with the limits from cashLimits.jsp
+     * @param diary amount limit
+     * @param monthly amount limit
+     * @throws Exception 
+     */
+    public void changeCashLimits(double diary, double monthly) throws Exception;
+    
+    /**
+     * Method that returns the card of the management
+     * @return
+     */
+    public Card getCard();
+
+}
